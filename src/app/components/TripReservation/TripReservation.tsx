223cc9390ad63489d506bfc85163a0654f87afd5
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-02-05 18:13:09 -0300

    feat: react hook form

--- src/app/components/TripReservation/TripReservation.tsx
+++ src/app/components/TripReservation/TripReservation.tsx
@@ -1,14 +1,24 @@
 "use client"
 import { Trip } from "@prisma/client";
 import React, { useState } from "react";
+import {useForm} from "react-hook-form";
 
 export type TripItemsProps = {
     trip: Trip;
 }
 
+type TripReservationForm = {
+    guests: number;
+}
+
 export default function TripReservation(props: TripItemsProps & { startDate: string, endDate: string }): React.JSX.Element {
+    const {register, handleSubmit, formState:{errors}} = useForm<TripReservationForm>();
     const [tripTotal, setTripTotal] = useState(0);
     const { trip, startDate, endDate } = props;
+
+    const onSubmit = (data:any)=>{
+        console.log({data});
+    }
 
     return (
         <div className="leading-7 mx-auto w-11/12 my-2">
@@ -16,14 +26,33 @@ export default function TripReservation(props: TripItemsProps & { startDate: str
                 <input className="p-2 rounded-lg w-2/4 mr-1 border-2 border-gray-300" placeholder="Data de inicio" type="date" min={startDate} max={endDate}></input>
                 <input className="p-2 rounded-lg w-2/4 ml-1 border-2 border-gray-300" placeholder="Data final" type="date" min={startDate} max={endDate}></input>
             </div>
-            <input placeholder={`Numero de hospedes (max:${trip.maxGuests || 'N/A'})`} className="p-2 rounded-lg w-full border-2 border-gray-300 mt-1" type="number" min="1" max="5" onChange={(e) => setTripTotal(Number(e.target.value))}></input>
+            <input 
+
+            {...register("guests", {
+                required:{
+                value:true,
+                message:"N° de hóspedes obrigatório!",
+            },
+            })}
+            error={!! errors?.guests}
+            errorMessage={errors?.guests?.message}
+            placeholder={`Numero de hospedes (max:${trip.maxGuests || 'N/A'})`} className="p-2 rounded-lg w-full border-2 border-gray-300 mt-1" type="number" min="1" max="5" onChange={(e) => setTripTotal(Number(e.target.value))}></input>
             <div className="flex font-bold w-full justify-between text-gray-500 my-2 mt-3">
                 <p>Total:</p>
                 <p>R$ {tripTotal * Number(trip.pricePerDay)}</p>
             </div>
-            <button className="w-full rounded-lg p-2 text-white bg-primary border-none mb-5">Reservar agora</button>
+            <button className="w-full rounded-lg p-2 text-white bg-primary border-none mb-5" onClick={()=>handleSubmit(onSubmit)()}>Reservar agora</button>
             <div className="bg-gray-400 w-full h-[2px] my-2"></div>
         </div>
     )
 }
 
+
+
+/*
+
+O input dele esta aparecendo o erro porque ele fez um input separado, verificar no react hook form como mostrar coisa similar
+
+
+
+*/
