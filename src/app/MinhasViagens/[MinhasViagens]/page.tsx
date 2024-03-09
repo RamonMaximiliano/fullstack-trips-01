f149673f714f433d05ed7e181d573bc753e95563
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-03-09 17:23:07 -0300

    bug: user get trips fix

--- src/app/MinhasViagens/[MinhasViagens]/page.tsx
+++ src/app/MinhasViagens/[MinhasViagens]/page.tsx
@@ -26,7 +26,8 @@ async function getReservation() {
 export default async function MinhasViagens({ params }: { params: { MinhasViagens: string } }) {
     const users: any = await getUsers();
     const trips: any = await getReservation();
-    const userEnd = params.MinhasViagens.replace("%20", " ")
+    //The below is a regular expression, the g means "global", it will replace everything inside // in the string by the empty space on ""   
+    const userEnd = params.MinhasViagens.replace(/%20/g, " ")
     const userID = users.find((item: User) => {
         return item.name == userEnd
     })
@@ -39,7 +40,7 @@ export default async function MinhasViagens({ params }: { params: { MinhasViagen
             <main className="flex-grow">
             <h1 className="font-bold my-4 lg:ml-60">Minhas viagens</h1>
             <div className="lg:flex lg:flex-wrap lg:gap-2 lg:w-4/6	lg:mx-auto justify-between">
-                {MyTrips.length <= 0 && <h2>Você não tem nenhuma viagem reservada, ou aguarde o sistema ser atualizado.<br/> Volte em alguns minutos!</h2>}
+                {MyTrips.length <= 0 && <h2>Você não tem nenhuma viagem reservada, ou aguarde o sistema ser atualizado.<br/>Volte em alguns minutos!</h2>}
             {
                 MyTrips.map((item: TripReservation) =>
                     <div className="my-4 shadow-xl p-4 rounded-xl bg-gray-100 lg:w-[320px]" key={item.id}>
